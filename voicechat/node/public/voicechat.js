68378621bbe4b5fefc0abe41d4b87ee9fcbcbde2
frog <162057394+forgman6@users.noreply.github.com>
2025-10-18 00:00:34 -0400

    test wroked

--- voicechat/node/public/voicechat.js
+++ voicechat/node/public/voicechat.js
@@ -36,13 +36,13 @@ const sessionId = urlParams.get('sessionId');
 const address = window.location.host;
 
 const ICE_SERVERS = [
-    // { urls: 'stun:stun.l.google.com:19302' },
-    // { urls: 'stun:stun1.l.google.com:19302' },
-    // { urls: 'stun:stun2.l.google.com:19302' },
-    // { urls: `turn:${window.location.hostname}:3478`,
-    //     credential: sessionId,
-    //     username: sessionId,
-    // }
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: `turn:${window.location.hostname}:3478`,
+        credential: sessionId,
+        username: sessionId,
+    }
 ]
 
 // Utility Functions
